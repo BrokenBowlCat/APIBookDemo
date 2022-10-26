65048b37285f59ad16ce240b505d639a555748d1
BrokenBowlCat <superbread77321@gmail.com>
2022-10-26 11:27:51 +0800

    APIDemo

---
+++ APIBookDemo/DogAPIStruc.swift
@@ -0,0 +1,12 @@
+//
+//  DogAPIStruc.swift
+//  APIBookDemo
+//
+//  Created by 03PKK on 2022/10/25.
+//
+
+import Foundation
+struct randomDog : Codable{
+    let image:URL
+    let fact:String
+}
--- APIBookDemo/ViewController.swift
+++ APIBookDemo/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dogImage: UIImageView!
+    @IBOutlet weak var dogLabel: UILabel!
+    @IBOutlet weak var changeButtom: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        callAPI()
+//        UIUpdate()
+    }
+    var randomDogClass:randomDog?
+    
+    func callAPI() {
+        let urlString = "https://some-random-api.ml/animal/dog"
+        if let url = URL(string: urlString) {
+            URLSession.shared.dataTask(with: url) { data, response , error in
+                if let data {
+                    let decoder = JSONDecoder()
+                    do {
+                        let randomDog = try decoder.decode(randomDog.self, from: data)
+//                        print(randomDog)
+                        DispatchQueue.main.async {
+//                            let temp = randomDog.fact
+//                            print(temp)
+                            self.dogLabel.text = randomDog.fact
+
+                            let sureUrl = randomDog.image
+                            URLSession.shared.dataTask(with: sureUrl) { data, response, error in
+                                if let data = data {
+                                        DispatchQueue.main.async {
+                                            self.dogImage.image =  UIImage(data: data)
+                                            
+                                    }
+                                }
+                            }.resume()
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
     }
 
-
+    @IBAction func againButton(_ sender: Any) {
+        callAPI()
+    }
 }
-
